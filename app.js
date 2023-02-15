948f0fd736f2b93d0dcc7e0305710230de3c9289
vse-opensource <virtualspace.suporte@gmail.com>
2023-02-15 01:09:33 -0300

    add mobile menu

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ var scrollTo = function(identifier, speed) {
         scrollTop: $(identifier).offset().top
     }, speed || 800);
 }
-$(document).scroll(function() {
-    var y = $(this).scrollTop();
-    if (y > 0) {
-        $('.menu').fadeOut(200);
-    } else {
-        $('.menu').fadeIn(200);
-    }
-});
+//$(document).scroll(function() {
+//    var y = $(this).scrollTop();
+//    if (y > 0) {
+//        $('.menu').fadeOut(200);
+//    } else {
+//        $('.menu').fadeIn(200);
+//    }
+//});
 
-function section1(){ scrollTo("#section1"); }
-function section2(){ scrollTo("#section2"); }
-function section3(){ scrollTo("#section3"); }
-function section4(){ scrollTo("#section4"); }
+function openMobileMenu(){ $(".mobile-menu-content").fadeIn(); }
+function section1(){ scrollTo("#section1"); $(".mobile-menu-content").fadeOut(); }
+function section2(){ scrollTo("#section2"); $(".mobile-menu-content").fadeOut(); }
+function section3(){ scrollTo("#section3"); $(".mobile-menu-content").fadeOut(); }
+function section4(){ scrollTo("#section4"); $(".mobile-menu-content").fadeOut(); }
